de78de21a7c70fe8d7437b077b02029eb1a3ca5c
fikri <mhfikri@outlook.com>
2023-03-16 22:30:13 +0700

    Add dht22 sensor.

---
+++ main/temperature.c
@@ -0,0 +1,37 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "esp_log.h"
+
+#include "dht.h"
+
+#include "context.h"
+#include "error.h"
+#include "temperature.h"
+
+static const char *TAG = "temperature";
+
+static void temperature_task(void *arg)
+{
+    context_t *context = (context_t *)arg;
+    ARG_ERROR_CHECK(context != NULL, ERR_PARAM_NULL);
+
+    float temperature, humidity;
+    while (true) {
+        if (dht_read_float_data(DHT_TYPE_AM2301, CONFIG_DHT22_DATA_GPIO, &humidity, &temperature) == ESP_OK) {
+            context_set_temp_humidity(context, temperature, humidity);
+            ESP_LOGI(TAG, "Temperature: %.1fC Humidity: %.1f%%", temperature, humidity);
+        } else {
+            ESP_LOGE(TAG, "Error cannot read data from DHT22.");
+        }
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+}
+
+esp_err_t temperature_init(context_t *context)
+{
+    ARG_CHECK(context != NULL, ERR_PARAM_NULL);
+
+    xTaskCreatePinnedToCore(temperature_task, "temperature", 2048, context, 10, NULL, tskNO_AFFINITY);
+    return ESP_OK;
+}
---
+++ main/temperature.h
@@ -0,0 +1,10 @@
+#ifndef IR_BLASTER_TEMPERATURE_H
+#define IR_BLASTER_TEMPERATURE_H
+
+#include "esp_err.h"
+
+#include "context.h"
+
+esp_err_t temperature_init(context_t *context);
+
+#endif // IR_BLASTER_TEMPERATURE_H
