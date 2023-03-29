aeb1e9a45eb793aee9669eb82735be5f9cfdde1b
fikri <mhfikri@outlook.com>
2023-03-30 00:05:30 +0700

    Add http sendCommand function.

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,9 +1,30 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const iot = require('@google-cloud/iot');
+const iotClient = new iot.v1.DeviceManagerClient();
+
+exports.httpSendCommand = functions.region('asia-southeast2').https.onRequest(async (req, res) => {
+  const projectId = 'ir-blaster-376915';
+  const region = 'asia-east1';
+  const registryId = 'irblaster';
+  const deviceId = req.body.deviceId;
+
+  const name = iotClient.devicePath(projectId, region, registryId, deviceId);
+  const data = Buffer.from(JSON.stringify(req.body)).toString('base64');
+
+  const request = {
+    name: name,
+    binaryData: data,
+  };
+  functions.logger.info(request);
+
+  try {
+    const [response] = await iotClient.sendCommandToDevice(request);
+    res.sendStatus(200);
+  } catch (e) {
+    functions.logger.error(e);
+    res.sendStatus(404);
+  }
+});
